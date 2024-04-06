cbeb17c18fad444c9d61e11650db11dc6071c82f
Haemin Park <hmin.park1@gmail.com>
2024-04-06 01:24:54 -0400

    hangman part1

--- src/loop/hangman_game.py
+++ src/loop/hangman_game.py
@@ -1,11 +1,100 @@
 import random
 
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+end_of_game = False
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
-guess = input("Guess a letter: ").lower
+lives = 6
 
-for letter in chosen_word:
-    if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+# create an empty list
+# For each letter in the chosen_word, add a "_" to the list
+display = []
+for _ in range(len(chosen_word)):
+    display.append("_")
+print(display)
+
+# ask user to guess a letter
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    #check guessed letter
+    for position in range(len(chosen_word)):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose!")
+
+    print(f"{' '.join(display)}")
+
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win!")
+
+    print(stages[lives])
