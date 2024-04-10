483dbc20f9e248c1e99035018236d76008eb940b
Haemin Park <hmin.park1@gmail.com>
2024-04-10 00:31:11 -0400

    parameter examples2

--- src/function_parameter/caesar_cipher.py
+++ src/function_parameter/caesar_cipher.py
@@ -1,23 +1,18 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-def encrypt(text, shift):
-    encrypted_text = ""
+def caeser(text, shift):
+    new_text = ""
+    if direction == "decode":
+        shift *= -1
+
     for letter in text:
         index = alphabet.index(letter)
-        index += shift
-        encrypted_text += alphabet[index]
-    return encrypted_text
+        new_index = index + shift
+        new_text += alphabet[new_index]
+    print(f"The {direction}d text is {new_text}")
 
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
-
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
-
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
-new_text = encrypt(text, shift)
-print(new_text)
+caeser(text, shift)
