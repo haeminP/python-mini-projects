601c471186aadbe4ec9502efe1cb8f77c5c4d4ab
Haemin Park <hmin.park1@gmail.com>
2024-04-10 23:18:07 -0400

    caesar cipher final version

--- src/function_parameter/caesar_cipher.py
+++ src/function_parameter/caesar_cipher.py
@@ -1,8 +1,7 @@
+from caesar_art import logo
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
 def caeser(text, shift):
     new_text = ""
@@ -10,9 +9,26 @@ def caeser(text, shift):
         shift *= -1
 
     for letter in text:
-        index = alphabet.index(letter)
-        new_index = index + shift
-        new_text += alphabet[new_index]
+        if letter in alphabet:
+            index = alphabet.index(letter)
+            new_index = index + shift
+            new_text += alphabet[new_index]
+        else:
+            new_text += letter
     print(f"The {direction}d text is {new_text}")
 
-caeser(text, shift)
+
+
+should_continue = True
+print(logo)
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    #in case user enters a shift that is greater than the number of letters in the alphabet
+    shift %= 26
+    caeser(text, shift)
+    result = input("Type 'yes' if you want to go again. Otherwise type 'no'\n")
+    if result == "no":
+        should_continue = False
+        print("Goodbye")
