a77585f5f9a59f767d09d2095d450e5eea965804
Haemin Park <hmin.park1@gmail.com>
2024-04-16 00:19:05 -0400

    secret auction version2

--- src/dictionary/secret_auction.py
+++ src/dictionary/secret_auction.py
@@ -1,14 +1,27 @@
-bid_continue = False
-bid_list = {}
+from art import logo
 
-while bid_continue:
-    name = input("What is your name?")
-    bid_price = int(input("How much would you bid?"))
-    bid_list[name] = bid_price
-    next_round = int("Is there another user who wants to bid?")
-    if next_round == "yes":
-        bid_continue = True
-    elif next_round == "no":
-        bid_continue = False
 
-print(bid_list)
+print(logo)
+
+bids = {}
+bidding_finished = False
+
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_record:
+        if highest_bid < bidding_record[bidder]:
+            highest_bid = bidding_record[bidder]
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+
+while not bidding_finished:
+    name = input("What is your name? ")
+    bid_price = int(input("How much would you bid? $"))
+    bids[name] = bid_price
+    should_continue = input("Are there any other bidders? Type yes or no\n")
+    if should_continue == "no":
+        bidding_finished = True
+        find_highest_bidder(bids)
+
