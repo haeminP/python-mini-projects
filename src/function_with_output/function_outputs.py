dce099dbf8f8f3a2f1d9be191a842fecc0944d16
Haemin Park <hmin.park1@gmail.com>
2024-04-18 23:56:15 -0400

    function with outputs 2

--- src/function_with_output/function_outputs.py
+++ src/function_with_output/function_outputs.py
@@ -1,6 +1,8 @@
 #Functions with outputs
 
 def format_name(f_name, l_name):
+    """Take a first and last name and format it
+    to return the title case version of the name"""
     if f_name == "" or l_name == "":
         return "You didn't provide valid inputs."
     formatted_f_name = f_name.title()
