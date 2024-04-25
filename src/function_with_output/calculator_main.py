ccc57c467c6727541f908329d62734de94263a1f
Haemin Park <hmin.park1@gmail.com>
2024-04-24 20:58:16 -0400

    calculator program update

--- src/function_with_output/calculator_main.py
+++ src/function_with_output/calculator_main.py
@@ -1,19 +1,22 @@
+
 from art import logo
+
 
 def add(n1, n2):
     return n1 + n2
 
+
 def subtract(n1, n2):
     return n1 - n2
+
 
 def multiply(n1, n2):
     return n1 * n2
 
+
 def divide(n1, n2):
-    if n2 == 0:
-        return
-    else:
-        return n1 / n2
+    return n1 / n2
+
 
 operations = {
     "+": add,
@@ -22,13 +25,27 @@ operations = {
     "/": divide
 }
 
-num1 = int(input("What's the first number?: "))
-for symbol in operations:
-    print(symbol, end=" ")
 
-print()
-operation_symbol = input("Pick an operation from the line above: ")
-function = operations[operation_symbol]
-num2 = int(input("What's the second number?: "))
-answer = function(num1, num2)
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+def calculator():
+    print(logo)
+
+    num1 = float(input("+"))
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
+
+    while should_continue:
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What's the next number?: "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+
+
+calculator()
