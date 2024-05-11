8acfbe90c5aba69cd60ed48fe7f4b95a93cc0b2d
Haemin Park <hmin.park1@gmail.com>
2024-05-10 23:57:37 -0400

    python while

--- src/function_with_output/blackjack_practice.py
+++ src/function_with_output/blackjack_practice.py
@@ -8,7 +8,9 @@ def deal_card():
 
 
 def calculate_score(cards):
-    """0 will represent a blackjack in our game.
+    """
+    Take a list of cards and return the score calculated from the cards
+    0 will represent a blackjack in our game.
     If the score is already over 21, remove the 11 and replace it with a 1"""
     if sum(cards) == 21 and len(cards) == 2:
         return 0
@@ -22,11 +24,29 @@ def calculate_score(cards):
 
 user_cards = []
 computer_cards = []
+is_game_over = False
 
+#deal cards to the user and dealer
 for _ in range(2):
     user_cards.append(deal_card())
     computer_cards.append(deal_card())
 
+while not is_game_over:
+    user_score = calculate_score(user_cards)
+    computer_score = calculate_score(computer_cards)
+    print(f"    Your cards: {user_cards}, current score: {user_score}")
+    print(f"    Computer's first card: {computer_cards[0]}")
 
-user_score = calculate_score(user_cards)
-computer_score = calculate_score(computer_cards)
+    if user_score == 0 or computer_score == 0 or user_score > 21:
+        is_game_over = True
+
+    else:
+        user_want_deal = input("Type 'y' to get another card, type 'n' to pass: ")
+        if user_want_deal == "y":
+            user_cards.append(deal_card())
+        else:
+            is_game_over = True
+
+while computer_score != 0 and computer_score < 17:
+    computer_cards.append(deal_card())
+    computer_score = calculate_score(computer_cards)
