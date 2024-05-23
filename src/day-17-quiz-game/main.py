15480e4a99fd3907a62499753afe56a1b2876e55
Haemin Park <hmin.park1@gmail.com>
2024-05-23 00:46:18 -0400

    oop2

---
+++ src/day-17-quiz-game/main.py
@@ -0,0 +1,13 @@
+from data import question_data
+from question_model import Question
+from quiz_brain import QuizBrain
+
+question_bank = []
+for question in question_data:
+    new_question = Question(question["text"], question["answer"])
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
