3738e7efb8435fc6aa6ca72188feb54903eca68b
Haemin Park <hmin.park1@gmail.com>
2024-05-24 23:48:35 -0400

    day18

---
+++ src/day18/main.py
@@ -0,0 +1,25 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+tim.shape("turtle")
+tim.color("SkyBlue")
+
+for _ in range(4):
+    tim.right(90)
+    tim.forward(100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
