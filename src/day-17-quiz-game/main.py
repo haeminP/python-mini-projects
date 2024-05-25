04d12ee053d5bcfa435ea75b7196b728cc38efdf
Haemin Park <hmin.park1@gmail.com>
2024-05-24 22:37:29 -0400

    day17

--- src/day-17-quiz-game/main.py
+++ src/day-17-quiz-game/main.py
@@ -1,4 +1,4 @@
-from data import question_data
+from data import question_data, question_data2
 from question_model import Question
 from quiz_brain import QuizBrain
 
@@ -9,5 +9,15 @@ for question in question_data:
 
 quiz = QuizBrain(question_bank)
 
+
+# question_bank2 = []
+# for question in question_data2:
+#     new_question = Question(question["question"], question["correct_answer"])
+#     question_bank2.append(new_question)
+# quiz2 = QuizBrain(question_bank2)
+
 while quiz.still_has_questions():
     quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
