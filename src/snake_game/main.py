32651ed31321d35f3ca680fae0c9236e69b2522e
Haemin Park <hmin.park1@gmail.com>
2024-05-28 14:35:58 -0400

    day20 - snake game - final version

--- src/snake_game/main.py
+++ src/snake_game/main.py
@@ -27,12 +27,26 @@ screen.update()
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.2)
+    time.sleep(0.15)
     snake.move()
 
     # Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
+
+    # Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        scoreboard.game_over()
+        game_is_on = False
+
+    # Detect collision with tail
+    # if head collides with any segment in the tail: trigger game over sequence
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
 
 screen.exitonclick()
