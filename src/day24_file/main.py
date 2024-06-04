37c1002aaf28dff173663c45f2ae39ab05761161
Haemin Park <hmin.park1@gmail.com>
2024-06-04 00:36:54 -0400

    file read/write - snake game update

--- src/day24_file/main.py
+++ src/day24_file/main.py
@@ -1,13 +1,23 @@
 # file = open("my_file.txt")
+# file.close()
+
 
 # default mode is read
 with open("my_file.txt") as file:
     contents = file.read()
     print(contents)
 
+# write mode
 with open("my_file.txt", mode="w") as file:
     file.write("I like pasta!")
 
-# file.close()
+# append mode
+with open("my_file.txt", mode="a") as file:
+    file.write("\nBy the way, I'm Haemin!")
+
+with open("my_file.txt") as file:
+    contents = file.read()
+    print(contents)
+
 
 
