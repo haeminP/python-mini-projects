87c2717f1ae5439d1703dacda4fc057d7b8b48e3
Haemin Park <hmin.park1@gmail.com>
2024-06-22 01:11:42 -0400

    us states game version2

--- src/us_states_game/main.py
+++ src/us_states_game/main.py
@@ -8,8 +8,7 @@ screen.title("U.S. States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?").title()
-print(answer_state)
+
 
 # read the data
 data = pandas.read_csv("50_states.csv")
@@ -18,6 +17,9 @@ data_dict = data.to_dict()
 states = list(data_dict["state"].values())
 x_coordinates = data_dict["x"]
 y_coordinates = data_dict["y"]
+
+
+score = 0
 
 
 def text_at_xy(x, y, text):
@@ -30,8 +32,19 @@ def text_at_xy(x, y, text):
 def get_mouse_click_coor(x, y):
     print(x, y)
 
-if answer_state in states:
-    text_at_xy(30, 50, answer_state)
+def check_answer(answer_state, score):
+    if answer_state in states:
+        index = states.index(answer_state)
+        x_coordinate = x_coordinates.get(index)
+        y_coordinate = y_coordinates.get(index)
+        text_at_xy(x_coordinate, y_coordinate, answer_state)
+        score += 1
+
+
+game_is_on = True
+while game_is_on:
+    answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?").title()
+    check_answer(answer_state, score)
 
 # turtle.onscreenclick(get_mouse_click_coor())
 # turtle.mainloop()
